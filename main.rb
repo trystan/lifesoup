bded14ac04bb05ae8d5765ab1a59c1644e38c494
Trystan Spangler <trystan.s@comcast.net>
2013-05-08 17:47:38 -0700

    a screen

--- main.rb
+++ main.rb
@@ -1,3 +1,20 @@
 
+require 'rubygame'
 
-puts "life soup!"
+screen = Rubygame::Screen.new [500, 500]
+screen.fill [0, 0, 16]
+screen.update
+
+queue = Rubygame::EventQueue.new
+
+running = true
+while running do
+  queue.each do |event|
+    case event
+      when Rubygame::QuitEvent
+        running = false
+    end
+  end
+end
+
+Rubygame.quit
