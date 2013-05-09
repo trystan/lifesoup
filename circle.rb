3c688060762b4327b6c489eedf1c8436ce3448ec
Trystan Spangler <trystan.s@comcast.net>
2013-05-08 18:10:18 -0700

    add brownian motion

--- circle.rb
+++ circle.rb
@@ -7,9 +7,13 @@ class Circle
     @velocity = [rand(3.0) - 1.0, rand(3.0) - 1.0]
     @color = [rand(128) + 64, rand(128) + 64, rand(128) + 64]
     @radius = 5
+    @max_speed = 2.0
   end
 
   def update
+    @velocity[0] = [[-@max_speed, @velocity[0] + (rand(3.0) - 1.0) / 10.0].max, @max_speed].min
+    @velocity[1] = [[-@max_speed, @velocity[1] + (rand(3.0) - 1.0) / 10.0].max, @max_speed].min
+
     @position[0] += @velocity[0]
     @position[1] += @velocity[1]
   end
